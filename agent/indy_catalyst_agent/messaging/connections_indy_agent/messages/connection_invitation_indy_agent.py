b14568ad19112cefb19cda64b7e436944eeb3555
Nicholas Rempel <nbrempel@gmail.com>
2019-02-05 15:44:49 -0800

    Duplicate connections messages
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

---
+++ agent/indy_catalyst_agent/messaging/connections_indy_agent/messages/connection_invitation_indy_agent.py
@@ -0,0 +1,54 @@
+"""
+Represents an invitation message for establishing connection.
+"""
+
+from base64 import b64encode
+from uuid import uuid4
+
+from marshmallow import Schema, fields, post_load
+
+from ...agent_message import AgentMessage
+from ...message_types import MessageTypes
+from ...validators import must_not_be_none
+
+from ..handlers.connection_invitation_handler import ConnectionInvitationHandler
+
+from .nested.agent_endpoint import AgentEndpoint, AgentEndpointSchema
+
+
+class ConnectionInvitation(AgentMessage):
+    def __init__(self, endpoint: str, label: str, key: str):
+        self.handler = ConnectionInvitationHandler(self)
+        self.endpoint = endpoint
+        self.label = label
+        self.key = key
+
+    @property
+    # Avoid clobbering builtin property
+    def _type(self) -> str:
+        return MessageTypes.CONNECTION_INVITE.value
+
+    @classmethod
+    def deserialize(cls, obj):
+        return ConnectionInvitationSchema().load(obj)
+
+    def serialize(self):
+        return ConnectionInvitationSchema().dump(self)
+
+    def base64_encode(self):
+        return b64encode(
+            bytes(ConnectionInvitationSchema().dumps(self), "utf8")
+        ).decode("utf8")
+
+
+class ConnectionInvitationSchema(Schema):
+    # Avoid clobbering builtin property
+    _type = fields.Str(data_key="@type", required=True)
+    endpoint = fields.Str(required=True)
+    label = fields.Str(required=True)
+    key = fields.Str(required=True)
+
+    @post_load
+    def make_model(self, data: dict) -> ConnectionInvitation:
+        del data["_type"]
+        return ConnectionInvitation(**data)
